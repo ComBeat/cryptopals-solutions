7c49dd28a9fdc69b9c755976f01acb66f39fff4a
ComBeat <15837468+ComBeat@users.noreply.github.com>
2023-12-17 01:13:56 +0100

    Solved s1c5

--- set1/c5.py
+++ set1/c5.py
@@ -15,37 +15,35 @@
 # Encrypt a bunch of stuff using your repeating-key XOR function. Encrypt your mail. Encrypt your password file. Your .sig file. Get a feel for it. I promise, we aren't wasting your time with this.
 import binascii
 
+to_encrypt = b"""Burning 'em, if you ain't quick and nimble
+I go crazy when I hear a cymbal"""
+key = b"ICE"
+expected_out = b"0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+decoded_expected_out = binascii.unhexlify(expected_out)
 
-def repeating_key_xor(s):
-    print(f"{s=}")
-    print(f"{type(s)=}")
-    key = "ICE"
-    hex_key = binascii.hexlify(bytes(key, 'utf-8'))
-    print(f"{hex_key=}")
-    print(f"{type(hex_key)=}")
 
-    #print(s ^ hex_key)
+def repeating_key_xor():
+    ctr = 0
+    result = bytearray()
 
-    return "TODO"
+    for b in to_encrypt:
+        result.append(b ^ key[ctr])
+        ctr += 1
+        if ctr >= 3:
+            ctr = 0
+
+    return binascii.hexlify(result)
 
 
 if __name__ == "__main__":
-    to_encrypt = str.split("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal", '\n')
-    expected_out = ["0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272",
-                    "a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"]
-    actual_out = []
-    #print(f"{expected_out[0]}")
-    #print(f"{type(expected_out[0])}")
+    actual_out = repeating_key_xor()
 
-    for i in to_encrypt:
-        actual_out.append(repeating_key_xor(i))
+    print(f"{expected_out=}")
+    print(f"{len(expected_out)=}")
+    print(f"{actual_out=}")
+    print(f"{len(actual_out)=}")
 
-    if False:
-        print(f"{expected_out=}")
-        print(f"{len(expected_out)=}")
-        print(f"{actual_out=}")
-        print(f"{len(actual_out)=}")
-        if expected_out == actual_out:
-            print("Yay")
-        else:
-            print("Unequal length!")
+    if expected_out == actual_out:
+        print("Yay")
+    else:
+        print("Unequal length!")
