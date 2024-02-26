0dcb4e57a4d7afc386283578b91cd1cca5ee6855
ComBeat <ps3de@web.de>
2024-02-26 14:20:35 +0100

    Implement hamming distance function

--- set1/c6.py
+++ set1/c6.py
@@ -18,6 +18,39 @@
 #     8) For each block, the single-byte XOR key that produces the best looking histogram is the repeating-key XOR key byte for that block. Put them together and you have the key.
 #
 # This code is going to turn out to be surprisingly useful later on. Breaking repeating-key XOR ("Vigenere") statistically is obviously an academic exercise, a "Crypto 101" thing. But more people "know how" to break it than can actually break it, and a similar technique breaks something much more important.
+import base64
+
+
+def hamming_distance(s1, s2):
+    distance = 0
+
+    if len(s1) != len(s2):
+        distance = -1
+        return distance
+
+    s1_decoded = base64.b64decode(s1)
+    s2_decoded = base64.b64decode(s2)
+
+    for byte1, byte2 in zip(s1_decoded, s2_decoded):
+        # XOR the bytes and count the differing bits
+        xor_result = byte1 ^ byte2
+        distance += bin(xor_result).count('1')
+
+    return distance
+
 
 if __name__ == '__main__':
-    keysize = [range(2, 41)]
+    keysize = range(2, 41)
+    test1 = "this is a test"
+    test2 = "wokka wokka!!!"
+    expected_difference = 37
+    actual_difference = hamming_distance(base64.b64encode(bytes(test1, 'utf-8')), base64.b64encode(bytes(test2, 'utf-8')))
+
+    with open('c6.txt') as f:
+        fcontent = f.readlines()
+
+    print(f"{expected_difference=}", f"{actual_difference=}")
+    if expected_difference == actual_difference:
+        print("Test Yay")
+    else:
+        print("Test Nooo")
